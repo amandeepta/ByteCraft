9eddab1c61e0af16274717539a071f13cf190f23
amandeepta <amandiptaitis@gmail.com>
2025-04-10 10:36:47 +0530

    Added the initial files

---
+++ src/ast.ts
@@ -0,0 +1,29 @@
+export type Node = "Program" | "NumericLiteral" | "Identifier" | "BinaryExp";
+
+export interface State {
+  kind: Node;
+}
+
+export interface Program extends State {
+  kind: "Program";
+  body: State[];
+}
+
+export interface Exp extends State {}
+
+export interface BinaryExp extends Exp {
+  kind: "BinaryExp";
+  left: Exp;
+  right: Exp;
+  op: string;
+}
+
+export interface Identifier extends Exp {
+  kind: "Identifier";
+  symbol: string;
+}
+
+export interface NumericLiteral extends Exp {
+  kind: "NumericLiteral";
+  value: number;
+}
---
+++ src/lex.ts
@@ -0,0 +1,60 @@
+export enum TokenType {
+  Number,
+  Identifier,
+  Equals,
+  OpenParen,
+  CloseParen,
+  BinaryOp,
+}
+
+export interface Token {
+  value: string;
+  type: TokenType;
+}
+
+function getToken(value: string = "", type: TokenType): Token {
+  return { value, type };
+}
+
+export function Tokenize(code: string): Token[] {
+  const tokens: Token[] = [];
+  const src = code.split("");
+
+  while (src.length > 0) {
+    const char = src[0];
+
+    if (char === "(") {
+      tokens.push(getToken(src.shift()!, TokenType.OpenParen));
+    } else if (char === ")") {
+      tokens.push(getToken(src.shift()!, TokenType.CloseParen));
+    } else if ("+-*/".includes(char)) {
+      tokens.push(getToken(src.shift()!, TokenType.BinaryOp));
+    } else if (char === "=") {
+      tokens.push(getToken(src.shift()!, TokenType.Equals));
+    } else if (/\d/.test(char)) {
+      let num = "";
+      while (src.length > 0 && /\d/.test(src[0])) {
+        num += src.shift();
+      }
+      tokens.push(getToken(num, TokenType.Number));
+    } else if (/[a-zA-Z]/.test(char)) {
+      let ident = "";
+      while (src.length > 0 && /[a-zA-Z]/.test(src[0])) {
+        ident += src.shift();
+      }
+      tokens.push(getToken(ident, TokenType.Identifier));
+    } else {
+      console.log("unrecognizable content:", char);
+      src.shift();
+    }
+  }
+
+  return tokens;
+}
+
+const input ="x+y= 24"
+
+const tokens = Tokenize(input);
+for (const token of tokens) {
+  console.log(token);
+}
